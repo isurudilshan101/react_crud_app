7fa09d30ee987f33a0ba9ef55585ab7ed2b7d83b
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-05 18:02:35 +0530

    create vehicle

---
+++ vehicle_crud/src/components/create-vehicle.cpmponent.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+class CreateVehicle extends Component{
+    render(){
+        return(
+            <div>
+                <p>
+                    Vehicle create compponent
+                </p>
+            </div>
+        );
+    };
+}
+
+export default CreateVehicle;
