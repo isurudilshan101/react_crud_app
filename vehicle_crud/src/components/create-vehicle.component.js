a7ed421e4fc7f59d47fa91b7136d0d112d18fca8
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-06 23:17:38 +0530

    create vehicle

--- vehicle_crud/src/components/create-vehicle.component.js
+++ vehicle_crud/src/components/create-vehicle.component.js
@@ -32,23 +32,38 @@ export default class CreateVehicle extends Component{
         this.setState({number: e.target.value});
     }
 
+    onSubmit(e){
+        e.preventDefault();
+
+        console.log('Vehicle Added');
+
+        console.log(`Vehicle Name: ${this.state.name}`);
+
+        console.log(`Vehicle Color: ${this.state.color}`);
+
+        console.log(`Vehicle Number: ${this.state. number}`);
+
+        this.setState({name:'', color: '' , number:''});
+
+    }
+
     render(){
         return(
             <div class="form-wrapper">
              <Form>
                  <Form.Group controlId="VehicleName">
                   <Form.Label>Name of the Vehicle: </Form.Label>   
-                  <Form.Control type="text"/>  
+                  <Form.Control type="text" value={this.state.name} onChange={this.onChangeVehicleName}/>  
                  </Form.Group>
 
                  <Form.Group controlId="Vehicle Color">
                   <Form.Label>Color of the Vehicle: </Form.Label>   
-                  <Form.Control type="text"/>  
+                  <Form.Control type="text" value={this.state.color} onChange={this. onChangeVehicleColor}/>  
                  </Form.Group>
 
                  <Form.Group controlId="Vehicle Number">
                   <Form.Label>Number of the Vehicle: </Form.Label>   
-                  <Form.Control type="text"/>  
+                  <Form.Control type="text" value={this.state.number} onChange={this. onChangeVehicleNumber}/>  
                  </Form.Group>
 
                 <Button variant="danger" size="lg" block="block" type="submit">Add Vehicle</Button>
