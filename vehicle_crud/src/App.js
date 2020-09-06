ee0235a32857d3dfa5ec0cb04be0f0143ceee2f2
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-06 13:38:17 +0530

    import files

--- vehicle_crud/src/App.js
+++ vehicle_crud/src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import CreateVehicle from './components/create-vehicle.cpmponent';
+import React,{Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
+import CreateVehicle from './components/create-vehicle.cpmponent';
+import EditVehicle from './components/etid-vehicle.component';
+import VehicleList from './components/vehicle-list.component';
 
-function App() {
+
+class App extends Component{
+
+  render(){
     return(
-      <div className="App">
-        <p>test</p>
-      </div>
-    );
+      
+        <div className="container">
+          <p>hhhhh</p>
+        </div>
+     
+    )
+  }
 }
+
 export default App;
