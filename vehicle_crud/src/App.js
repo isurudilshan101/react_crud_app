cf92c29970f4d2e1660a00fba33299ec65727d33
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-06 14:15:32 +0530

    home

--- vehicle_crud/src/App.js
+++ vehicle_crud/src/App.js
@@ -16,7 +16,30 @@ class App extends Component{
           <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
          <Link to={'/'} className="navbar-brand">React Vehicle CRUD by Isuru</Link>
+         <div className="collapse navbar-collaps" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-link">
+              <Link to={'/'} className="nav-link">Home</Link>
+            </li>
+
+            <li className="nav-link">
+              <Link to={'/'} className="nav-link">Edit Vehicle</Link>
+            </li>
+
+            <li className="nav-link">
+              <Link to={'/edit-vehicle/:id'} className="nav-link">Create Vehicle</Link>
+            </li>
+
+            <li className="nav-link">
+              <Link to={'/vehicle-list'} className="nav-link">Vehicle List</Link>
+            </li>
+            
+          </ul>
+         </div>
+         <br/>
+         <h2>Welcome!</h2>
         </nav>
+
         </div>
        </Router>
      
