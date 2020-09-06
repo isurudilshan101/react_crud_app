4fde2ecb6d5fe5e09b297999d2226d893027a807
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-07 01:19:43 +0530

    creating vehicle

---
+++ vehicle_crud/backend/routes/vehicle.route.js
@@ -0,0 +1,23 @@
+let mongoose=require('mongoose');
+let expresss=require('express');
+let router=express.Router();
+
+//importing vehicle model
+
+let vehicleScchema=require('../Models/Vehicle');
+
+//creating the vehicle 
+
+router.route('/create-vehicle').post((req,res,next)=>{
+    vehicleScchema.create(req,body,(error,data)=>{
+        if(error){
+            return next(error)
+        }else{
+            console.log(data)
+            res.json(data)
+        }
+    })
+});
+
+
+//reading vehicle
