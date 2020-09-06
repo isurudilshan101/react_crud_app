3c71fe411ed3a61d98e68915b156c31977ec4bcf
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-06 13:14:18 +0530

    create vehicle

--- vehicle_crud/src/App.js
+++ vehicle_crud/src/App.js
@@ -5,25 +5,10 @@ import CreateVehicle from './components/create-vehicle.cpmponent';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <CreateVehicle/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+      <div className="App">
+        <p>test</p>
+      </div>
+    );
 }
-
 export default App;
--- vehicle_crud/src/index.js
+++ vehicle_crud/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrouserRouter>
+  <BrowserRouter>
     <App/>
-  </BrouserRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
