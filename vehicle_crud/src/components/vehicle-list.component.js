340949f44184bb0940ef58503b854d350f31a029
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-10-09 12:24:23 +0530

    vehicle list

--- vehicle_crud/src/components/vehicle-list.component.js
+++ vehicle_crud/src/components/vehicle-list.component.js
@@ -1,6 +1,35 @@
 import React,{Component} from 'react';
+import axios from 'axios';
+import Table from 'react-boostrap/Table';
+import VehicleTableRow from './VehicleTableRow';
 
 export default class VehicleList extends Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+            vehicles: []
+        };
+
+        this.componentDidMount(){
+            axios.get('http://localhost:4000/vehicles')
+            .then(res=>{
+                this.setState({
+                        vehicles:res.data
+                });
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+        }
+        DataTable(){
+            return this.state.vehicles.map((res,i)=>{
+                return <VehicleTableRow obj={res} key={}i/>
+            });
+        }
+    }
+
+
     render(){
         return(
             <div>
